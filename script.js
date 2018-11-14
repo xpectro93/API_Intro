fcd81dfe77c53c9ed2cbf201350985b0ad4649a3
your name here <your_email_here>
2018-11-14 17:00:45 -0500

    2nd

--- script.js
+++ script.js
@@ -1,132 +1,169 @@
-let APIResponse = {
-  status: "success",
-  message: {
-    affenpinscher: [],
-    african: [],
-    airedale: [],
-    akita: [],
-    appenzeller: [],
-    basenji: [],
-    beagle: [],
-    bluetick: [],
-    borzoi: [],
-    bouvier: [],
-    boxer: [],
-    brabancon: [],
-    briard: [],
-    bulldog: ["boston", "french"],
-    bullterrier: ["staffordshire"],
-    cairn: [],
-    cattledog: ["australian"],
-    chihuahua: [],
-    chow: [],
-    clumber: [],
-    cockapoo: [],
-    collie: ["border"],
-    coonhound: [],
-    corgi: ["cardigan"],
-    cotondetulear: [],
-    dachshund: [],
-    dalmatian: [],
-    dane: ["great"],
-    deerhound: ["scottish"],
-    dhole: [],
-    dingo: [],
-    doberman: [],
-    elkhound: ["norwegian"],
-    entlebucher: [],
-    eskimo: [],
-    frise: ["bichon"],
-    germanshepherd: [],
-    greyhound: ["italian"],
-    groenendael: [],
-    hound: ["afghan", "basset", "blood", "english", "ibizan", "walker"],
-    husky: [],
-    keeshond: [],
-    kelpie: [],
-    komondor: [],
-    kuvasz: [],
-    labrador: [],
-    leonberg: [],
-    lhasa: [],
-    malamute: [],
-    malinois: [],
-    maltese: [],
-    mastiff: ["bull", "tibetan"],
-    mexicanhairless: [],
-    mix: [],
-    mountain: ["bernese", "swiss"],
-    newfoundland: [],
-    otterhound: [],
-    papillon: [],
-    pekinese: [],
-    pembroke: [],
-    pinscher: ["miniature"],
-    pointer: ["german", "germanlonghair"],
-    pomeranian: [],
-    poodle: ["miniature", "standard", "toy"],
-    pug: [],
-    puggle: [],
-    pyrenees: [],
-    redbone: [],
-    retriever: ["chesapeake", "curly", "flatcoated", "golden"],
-    ridgeback: ["rhodesian"],
-    rottweiler: [],
-    saluki: [],
-    samoyed: [],
-    schipperke: [],
-    schnauzer: ["giant", "miniature"],
-    setter: ["english", "gordon", "irish"],
-    sheepdog: ["english", "shetland"],
-    shiba: [],
-    shihtzu: [],
-    spaniel: [
-      "blenheim",
-      "brittany",
-      "cocker",
-      "irish",
-      "japanese",
-      "sussex",
-      "welsh"
-    ],
-    springer: ["english"],
-    stbernard: [],
-    terrier: [
-      "american",
-      "australian",
-      "bedlington",
-      "border",
-      "dandie",
-      "fox",
-      "irish",
-      "kerryblue",
-      "lakeland",
-      "norfolk",
-      "norwich",
-      "patterdale",
-      "russell",
-      "scottish",
-      "sealyham",
-      "silky",
-      "tibetan",
-      "toy",
-      "westhighland",
-      "wheaten",
-      "yorkshire"
-    ],
-    vizsla: [],
-    weimaraner: [],
-    whippet: [],
-    wolfhound: ["irish"]
+document.addEventListener("DOMContentLoaded", () => {
+  let button = document.querySelector("button");
+  let buttonrem = document.querySelector("button");
+  // let new_element_p = document.createElement("p");
+
+  button.addEventListener("click", () => {
+    let new_element_p = document.createElement("p");
+    new_element_p.innerText =
+      breedlist[Math.floor(Math.random() * breedlist.length)];
+    event.target.parentNode.appendChild(new_element_p, event.target);
+
+    buttonrem.addEventListener("click", () => {
+      event.target.parentNode
+        .appendChild(new_element_p, event.target)
+        .remove(new_element_p, event.target);
+    });
+  });
+
+  ///
+  let APIResponse = {
+    status: "success",
+    message: {
+      affenpinscher: [],
+      african: [],
+      airedale: [],
+      akita: [],
+      appenzeller: [],
+      basenji: [],
+      beagle: [],
+      bluetick: [],
+      borzoi: [],
+      bouvier: [],
+      boxer: [],
+      brabancon: [],
+      briard: [],
+      bulldog: ["boston", "french"],
+      bullterrier: ["staffordshire"],
+      cairn: [],
+      cattledog: ["australian"],
+      chihuahua: [],
+      chow: [],
+      clumber: [],
+      cockapoo: [],
+      collie: ["border"],
+      coonhound: [],
+      corgi: ["cardigan"],
+      cotondetulear: [],
+      dachshund: [],
+      dalmatian: [],
+      dane: ["great"],
+      deerhound: ["scottish"],
+      dhole: [],
+      dingo: [],
+      doberman: [],
+      elkhound: ["norwegian"],
+      entlebucher: [],
+      eskimo: [],
+      frise: ["bichon"],
+      germanshepherd: [],
+      greyhound: ["italian"],
+      groenendael: [],
+      hound: ["afghan", "basset", "blood", "english", "ibizan", "walker"],
+      husky: [],
+      keeshond: [],
+      kelpie: [],
+      komondor: [],
+      kuvasz: [],
+      labrador: [],
+      leonberg: [],
+      lhasa: [],
+      malamute: [],
+      malinois: [],
+      maltese: [],
+      mastiff: ["bull", "tibetan"],
+      mexicanhairless: [],
+      mix: [],
+      mountain: ["bernese", "swiss"],
+      newfoundland: [],
+      otterhound: [],
+      papillon: [],
+      pekinese: [],
+      pembroke: [],
+      pinscher: ["miniature"],
+      pointer: ["german", "germanlonghair"],
+      pomeranian: [],
+      poodle: ["miniature", "standard", "toy"],
+      pug: [],
+      puggle: [],
+      pyrenees: [],
+      redbone: [],
+      retriever: ["chesapeake", "curly", "flatcoated", "golden"],
+      ridgeback: ["rhodesian"],
+      rottweiler: [],
+      saluki: [],
+      samoyed: [],
+      schipperke: [],
+      schnauzer: ["giant", "miniature"],
+      setter: ["english", "gordon", "irish"],
+      sheepdog: ["english", "shetland"],
+      shiba: [],
+      shihtzu: [],
+      spaniel: [
+        "blenheim",
+        "brittany",
+        "cocker",
+        "irish",
+        "japanese",
+        "sussex",
+        "welsh"
+      ],
+      springer: ["english"],
+      stbernard: [],
+      terrier: [
+        "american",
+        "australian",
+        "bedlington",
+        "border",
+        "dandie",
+        "fox",
+        "irish",
+        "kerryblue",
+        "lakeland",
+        "norfolk",
+        "norwich",
+        "patterdale",
+        "russell",
+        "scottish",
+        "sealyham",
+        "silky",
+        "tibetan",
+        "toy",
+        "westhighland",
+        "wheaten",
+        "yorkshire"
+      ],
+      vizsla: [],
+      weimaraner: [],
+      whippet: [],
+      wolfhound: ["irish"]
+    }
+  };
+  //if sub breed:append to main breed
+  //else just output breed
+  //Capitalize first letter of word.
+
+  let breedlist = [];
+  let data = APIResponse.message;
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
-};
-//if sub breed:append to main breed
-//else just output breed
-//Capitalize first letter of word.
 
-let breedlist = [];
-breedlist.push(APIResponse["message"]);
+  Object.keys(data).forEach(animal => {
+    if (data[animal].length) {
+      data[animal].forEach(type => {
+        breedlist.push(
+          capitalizeFirstLetter(type) + " " + capitalizeFirstLetter(animal)
+        );
+        // console.log(type + " " + animal);
+      });
+    } else {
+      breedlist.push(capitalizeFirstLetter(animal));
+      // console.log(animal);
+    }
+  });
 
-// console.log(Object.values(APIResponse.message));
-console.log(breedlist);
-//interesting
+  // console.log(Object.values(APIResponse.message));
+  //interesting
+});
